530b42ea17248d9d7148edad8cdefd55bb3cf496
xarantolus <xarantolus@protonmail.com>
2023-04-21 09:01:35 +0000

    LLVM 15, Inkwell

--- src/main.rs
+++ src/main.rs
@@ -1,86 +1,68 @@
-extern crate llvm_sys as llvm;
+use inkwell::builder::Builder;
+use inkwell::context::Context;
+use inkwell::execution_engine::{ExecutionEngine, JitFunction};
+use inkwell::module::Module;
+use inkwell::OptimizationLevel;
+use std::error::Error;
 
-use std::ffi::CStr;
-use std::mem;
+/// Convenience type alias for the `sum` function.
+///
+/// Calling this is innately `unsafe` because there's no guarantee it doesn't
+/// do `unsafe` operations internally.
+type SumFunc = unsafe extern "C" fn(u64, u64, u64) -> u64;
 
-use llvm::core::*;
-use llvm::execution_engine::*;
-use llvm::target::*;
+struct CodeGen<'ctx> {
+    context: &'ctx Context,
+    module: Module<'ctx>,
+    builder: Builder<'ctx>,
+    execution_engine: ExecutionEngine<'ctx>,
+}
 
-fn main() {
-    unsafe {
-        // Set up a context, module and builder in that context.
-        let context = LLVMContextCreate();
-        let module = LLVMModuleCreateWithNameInContext(b"sum\0".as_ptr() as *const _, context);
-        let builder = LLVMCreateBuilderInContext(context);
+impl<'ctx> CodeGen<'ctx> {
+    fn jit_compile_sum(&self) -> Option<JitFunction<SumFunc>> {
+        let i64_type = self.context.i64_type();
+        let fn_type = i64_type.fn_type(&[i64_type.into(), i64_type.into(), i64_type.into()], false);
+        let function = self.module.add_function("sum", fn_type, None);
+        let basic_block = self.context.append_basic_block(function, "entry");
 
-        // get a type for sum function
-        let i64t = LLVMInt64TypeInContext(context);
-        let mut argts = [i64t, i64t, i64t];
-        let function_type = LLVMFunctionType(i64t, argts.as_mut_ptr(), argts.len() as u32, 0);
+        self.builder.position_at_end(basic_block);
 
-        // add it to our module
-        let function = LLVMAddFunction(module, b"sum\0".as_ptr() as *const _, function_type);
+        let x = function.get_nth_param(0)?.into_int_value();
+        let y = function.get_nth_param(1)?.into_int_value();
+        let z = function.get_nth_param(2)?.into_int_value();
 
-        // Create a basic block in the function and set our builder to generate
-        // code in it.
-        let bb = LLVMAppendBasicBlockInContext(context, function, b"entry\0".as_ptr() as *const _);
+        let sum = self.builder.build_int_add(x, y, "sum");
+        let sum = self.builder.build_int_add(sum, z, "sum");
 
-        LLVMPositionBuilderAtEnd(builder, bb);
+        self.builder.build_return(Some(&sum));
 
-        // get the function's arguments
-        let x = LLVMGetParam(function, 0);
-        let y = LLVMGetParam(function, 1);
-        let z = LLVMGetParam(function, 2);
-
-        let sum = LLVMBuildAdd(builder, x, y, b"sum.1\0".as_ptr() as *const _);
-        let sum = LLVMBuildAdd(builder, sum, z, b"sum.2\0".as_ptr() as *const _);
-
-        // Emit a `ret i64` into the function to return the computed sum.
-        LLVMBuildRet(builder, sum);
-
-        // done building
-        LLVMDisposeBuilder(builder);
-
-        // Dump the module as IR to stdout.
-        LLVMDumpModule(module);
-
-        // Robust code should check that these calls complete successfully.
-        // Each of calls is necessary to setup an execution engine which
-        // compiles to native code.
-        LLVMLinkInMCJIT();
-        LLVM_InitializeNativeTarget();
-        LLVM_InitializeNativeAsmPrinter();
-
-        // Build an execution engine.
-        let ee = {
-            let mut ee = mem::MaybeUninit::uninit();
-            let mut err = mem::zeroed();
-
-            // This moves ownership of the module into the execution engine.
-            if LLVMCreateExecutionEngineForModule(ee.as_mut_ptr(), module, &mut err) != 0 {
-                // In case of error, we must avoid using the uninitialized ExecutionEngineRef.
-                assert!(!err.is_null());
-                panic!("Failed to create execution engine: {:?}", CStr::from_ptr(err));
-            }
-
-            ee.assume_init()
-        };
-
-
-        let addr = LLVMGetFunctionAddress(ee, b"sum\0".as_ptr() as *const _);
-
-        let f: extern "C" fn(u64, u64, u64) -> u64 = mem::transmute(addr);
-
-        let x: u64 = 1;
-        let y: u64 = 1;
-        let z: u64 = 1;
-        let res = f(x, y, z);
-
-        println!("{} + {} + {} = {}", x, y, z, res);
-
-        // Clean up the rest.
-        LLVMDisposeExecutionEngine(ee);
-        LLVMContextDispose(context);
+        unsafe { self.execution_engine.get_function("sum").ok() }
     }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let context = Context::create();
+    let module = context.create_module("sum");
+    let execution_engine = module.create_jit_execution_engine(OptimizationLevel::None)?;
+    let codegen = CodeGen {
+        context: &context,
+        module,
+        builder: context.create_builder(),
+        execution_engine,
+    };
+
+    let sum = codegen
+        .jit_compile_sum()
+        .ok_or("Unable to JIT compile `sum`")?;
+
+    let x = 1u64;
+    let y = 2u64;
+    let z = 3u64;
+
+    unsafe {
+        println!("{} + {} + {} = {}", x, y, z, sum.call(x, y, z));
+        assert_eq!(sum.call(x, y, z), x + y + z);
+    }
+
+    Ok(())
 }
